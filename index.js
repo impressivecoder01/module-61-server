8177a4ad9de045bb334fab42c3cc2b1f1dadd3e0
impressivecoder01 <rohanrahman251@gmail.com>
2024-12-30 01:14:18 +0600

    logout apijwt

--- index.js
+++ index.js
@@ -58,6 +58,14 @@ async function run() {
                 secure: false
             })
             .send({success: true})
+            app.post('/logout', (req, res)=> {
+                res
+                .clearCookie('token', {
+                    httpOnly: true,
+                    secure: false
+                })
+                .send({success: true})
+            })
         })
 
         // jobs related APIs
